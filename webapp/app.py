533d0b4b655dfbaaeed1998d1c509bc05b803e21
jerome-acad-opswerks <jerome.evangelista@academy.opswerks.com>
2025-01-04 10:04:36 +0800

    Added Python prometheus-flask-exporter

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def main():
